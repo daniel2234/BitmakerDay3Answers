9a108536c3420e3cbd20740f3a91e028fdf6019d
Daniel Kwiatkowski <kwiatkowski.daniel@gmail.com>
2015-04-08 13:01:25 -0400

    first commit

---
+++ Question1/Question1/Question1/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  Question1
+//
+//  Created by Daniel Kwiatkowski on 2015-04-08.
+//  Copyright (c) 2015 Daniel Kwiatkowski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
